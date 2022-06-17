80dc339f0c363a110f327c015c5683dd39d541e9
marcuspeh <hkmpeh@gmail.com>
2022-06-17 15:58:40 +0800

    update port

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import app from './app'
 import { dataSource } from './data-source'
 
-const PORT:number = 3000
-
 dataSource.initialize()
   .then(() => {
-    app.listen(PORT)
-    console.log(`Listening on port ${PORT}`)
+    app.listen(process.env.PORT || 3000)
   })
   .catch(console.error)
