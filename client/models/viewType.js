83df72001511d3c3e972014a68263c61ce6717af
marcuspeh <hkmpeh@gmail.com>
2022-06-21 12:34:53 +0800

    Remove js files

--- client/models/viewType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ViewType = void 0;
-var ViewType;
-(function (ViewType) {
-    ViewType["VIEW_ALL"] = "View All";
-    ViewType["VIEW_DONE"] = "View Done";
-    ViewType["VIEW_UNDONE"] = "View Pending";
-})(ViewType = exports.ViewType || (exports.ViewType = {}));
--- client/utilities/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_CSRF_TOKEN = exports.USER_TOKEN = void 0;
-exports.USER_TOKEN = "GIN";
-exports.USER_CSRF_TOKEN = "TONIC";
