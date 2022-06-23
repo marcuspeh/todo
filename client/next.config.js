f3bbe47ac10863354b73529fc4975a3a44ea8c1c
marcuspeh <hkmpeh@gmail.com>
2022-06-23 15:22:50 +0800

    Update token check

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BACKEND_URL: "https://protected-scrubland.herokuapp.com/api",
-    FRONTEND_URL: "https://todo-lime-theta.vercel.app/",
+    BACKEND_URL: "https://todo-manager-2.herokuapp.com/api",
+    FRONTEND_URL: "https://todo-manager-2.herokuapp.com",
     API_KEY: "VERY SECRET KEY"
   }
 }
